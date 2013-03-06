3036af0d9b9ca1c4f0fcc3e04dd626dca1929dda
Danis Tazetdinov <d.tazetdinov@gmail.com>
2013-03-06 17:54:31 +0400

    added grouping for calendars

--- iRemember/iRemember/IRCalendarsViewController.m
+++ iRemember/iRemember/IRCalendarsViewController.m
@@ -14,32 +14,26 @@
 
 @interface IRCalendarsViewController () <UIDataSourceModelAssociation>
 
-@property (nonatomic, strong) NSArray *calendars;
+@property (nonatomic, strong) NSArray *sources;
+@property (nonatomic, strong) NSDictionary *calendars;
 
 -(void)becomeActive:(NSNotification*)notification;
--(void)accessGramted:(NSNotification*)notification;
+-(void)accessGranted:(NSNotification*)notification;
 
 @end
 
 @implementation IRCalendarsViewController
 
--(void)encodeRestorableStateWithCoder:(NSCoder *)coder
-{
-    DLog(@"coder");
-    
-    [super encodeRestorableStateWithCoder:coder];
-}
-
 -(void)decodeRestorableStateWithCoder:(NSCoder *)coder
 {
     [super decodeRestorableStateWithCoder:coder];
-    DLog(@"coder");
+    [self fillCalendars];
 }
 
 - (NSString *) modelIdentifierForElementAtIndexPath:(NSIndexPath *)idx inView:(UIView *)view
 {
     DLog(@"idx: %@, %@", idx, self.calendars);
-    EKCalendar *calendar = [self.calendars objectAtIndex:idx.row];
+    EKCalendar *calendar = self.calendars[[self.sources[idx.section] sourceIdentifier]] [idx.row];
     return calendar.calendarIdentifier;
 }
 
@@ -47,25 +41,28 @@
 {
     DLog(@"identifier %@, %@", identifier, self.calendars);
     NSIndexPath * __block indexPath;
-    [self.calendars enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-        EKCalendar *calendar = obj;
-        if ([calendar.calendarIdentifier isEqualToString:identifier])
-        {
-            indexPath = [NSIndexPath indexPathForRow:idx inSection:0];
-            *stop = YES;
-        }
+    [self.sources enumerateObjectsUsingBlock:^(id obj, NSUInteger sourceIdx, BOOL *sourceStop) {
+        EKSource *source = obj;
+        [self.calendars[source.sourceIdentifier] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            EKCalendar *calendar = obj;
+            if ([calendar.calendarIdentifier isEqualToString:identifier])
+            {
+                indexPath = [NSIndexPath indexPathForRow:idx inSection:sourceIdx];
+                *stop = YES;
+                *sourceStop = YES;
+            }
+        }];
     }];
     
     return indexPath;
 }
-
 
 -(void)becomeActive:(NSNotification*)notification
 {
     [self refresh];
 }
 
--(void)accessGramted:(NSNotification*)notification
+-(void)accessGranted:(NSNotification*)notification
 {
     [self refresh];
 }
@@ -109,11 +106,35 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
+-(void)fillCalendars
+{
+    NSMutableArray *sources = [NSMutableArray array];
+    NSArray *calendarsArray = [[IRReminderManager defaultManager] reminderCalendars];
+    NSMutableDictionary *calendars = [NSMutableDictionary dictionary];
+    [calendarsArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        EKCalendar *calendar = obj;
+        NSMutableArray *calendarsForSource = calendars[calendar.source.sourceIdentifier];
+        if (!calendarsForSource)
+        {
+            calendarsForSource = [NSMutableArray array];
+        }
+        if (![sources containsObject:calendar.source])
+        {
+            [sources addObject:calendar.source];
+        }
+        [calendarsForSource addObject:calendar];
+        calendars[calendar.source.sourceIdentifier] = calendarsForSource;
+    }];
+    self.sources = sources;
+    self.calendars = calendars;
+}
+
 -(IBAction)refresh
 {
     [self.refreshControl beginRefreshing];
 
-    self.calendars = [[IRReminderManager defaultManager] reminderCalendars];
+    [self fillCalendars];
+    
     self.navigationItem.rightBarButtonItem.enabled = [IRReminderManager defaultManager].accessGranted;
     
     [self.tableView reloadData];
@@ -125,12 +146,12 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+    return self.sources.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.calendars.count;
+    return [self.calendars[[self.sources[section] sourceIdentifier]] count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -138,41 +159,46 @@
     static NSString *CellIdentifier = @"CalendarCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
-    EKCalendar *calendar = self.calendars[indexPath.row];
+    EKCalendar *calendar = self.calendars[[self.sources[indexPath.section] sourceIdentifier]] [indexPath.row];
     
     cell.textLabel.text = calendar.title;
-    cell.detailTextLabel.text = calendar.source.title;
     
     return cell;
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return [self.sources[section] title];
 }
 
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
-    return YES;
+    return NO;
 }
 
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete)
-    {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert)
-    {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
+//// Override to support editing the table view.
+//- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    if (editingStyle == UITableViewCellEditingStyleDelete)
+//    {
+//        // Delete the row from the data source
+//        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+//    }   
+//    else if (editingStyle == UITableViewCellEditingStyleInsert)
+//    {
+//        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+//    }   
+//}
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([segue.identifier isEqualToString:@"ShowReminders"])
     {
         IRRemindersViewController *vc = segue.destinationViewController;
-        vc.calendarIdentifier = [self.calendars[[self.tableView indexPathForCell:sender].row] calendarIdentifier];
+        NSIndexPath *indexPath = [self.tableView indexPathForCell:sender];
+        vc.calendarIdentifier = [self.calendars[[self.sources[indexPath.section] sourceIdentifier]] [indexPath.row] calendarIdentifier];
     }
 }
 
